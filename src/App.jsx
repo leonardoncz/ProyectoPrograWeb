e843f9fe6bda6028084a11afa77a1bc56f801f4a
JorgeTQB <j.quispe.b1304@gmail.com>
2025-10-11 19:07:56 -0500

    #001 Arreglo de bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-// En App.jsx, importa los nuevos componentes
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import Login from './components/usuario/Login';
+import Registro from './components/usuario/Registro';
+import RecuperarContraseña from './components/usuario/RecuperarContraseña';
+import PanelUsuario from './components/usuario/PanelUsuario';
+import DetalleOrden from './components/usuario/DetalleOrden';
+import EditarPerfil from './components/usuario/EditarPerfil';
 import GestionUsuarios from './components/admin/GestionUsuarios';
 import GestionOrdenes from './components/admin/GestionOrdenes';
 import DetalleUsuarioAdmin from './components/admin/DetalleUsuarioAdmin';
 import DetalleOrdenAdmin from './components/admin/DetalleOrdenAdmin';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Tus rutas existentes */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registro" element={<Registro />} />
@@ -17,12 +24,10 @@ function App() {
         <Route path="/orden/:id" element={<DetalleOrden />} />
         <Route path="/perfil/editar" element={<EditarPerfil />} />
 
-        {/* NUEVAS RUTAS DE ADMINISTRADOR */}
         <Route path="/admin/usuarios" element={<GestionUsuarios />} />
         <Route path="/admin/usuario/:id" element={<DetalleUsuarioAdmin />} />
         <Route path="/admin/ordenes" element={<GestionOrdenes />} />
         <Route path="/admin/orden/:id" element={<DetalleOrdenAdmin />} />
-
       </Routes>
     </Router>
   );
